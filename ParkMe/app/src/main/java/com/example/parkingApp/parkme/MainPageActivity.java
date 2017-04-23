33ee37ca76ea6730ba37467f42d06ae8b5b507b4
Bosko Sogura <bolesogura93@gmail.com>
2017-04-23 16:43:50 +0200

    Rezervacija parking mesta (layout). Prikaz selektovanog datuma i vremena.

--- ParkMe/app/src/main/java/com/example/parkingApp/parkme/MainPageActivity.java
+++ ParkMe/app/src/main/java/com/example/parkingApp/parkme/MainPageActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainPageActivity extends AppCompatActivity {
 
@@ -28,5 +29,9 @@ public class MainPageActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        if(this.getIntent().getExtras() != null){
+            Toast.makeText(this, this.getIntent().getExtras().getString("value"),Toast.LENGTH_LONG).show();
+        }
     }
 }
--- ParkMe/app/src/main/java/com/example/parkingApp/parkme/ReservationActivity.java
+++ ParkMe/app/src/main/java/com/example/parkingApp/parkme/ReservationActivity.java
@@ -3,11 +3,17 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 
 import com.android.datetimepicker.date.DatePickerDialog;
@@ -22,17 +28,34 @@ public class ReservationActivity extends AppCompatActivity implements DatePicker
     private Calendar calendar;
     private java.text.DateFormat dateFormat;
     private SimpleDateFormat timeFormat;
+    EditText dropDate;
+    EditText endDate;
+    private int whichEditTextIsPressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
 
-        EditText dropDate = (EditText) findViewById(R.id.dateDrop);
+        dropDate = (EditText) findViewById(R.id.dateDrop);
+        endDate = (EditText) findViewById(R.id.dateEnd);
+        Button confirmRes = (Button) findViewById(R.id.confirmReservation);
 
         calendar = Calendar.getInstance();
         dateFormat = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault());
         timeFormat = new SimpleDateFormat(TIME_PATTERN, Locale.getDefault());
+
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter
+                .createFromResource(this, R.array.payment_array,
+                        android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        staticAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        dropdown.setAdapter(staticAdapter);
 
         dropDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +63,42 @@ public class ReservationActivity extends AppCompatActivity implements DatePicker
                 DatePickerDialog.newInstance(ReservationActivity.this, calendar.get(Calendar.YEAR),
                                 calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
                         .show(getFragmentManager(), "datePicker");
+            }
+        });
+
+        endDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog.newInstance(ReservationActivity.this, calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+                        .show(getFragmentManager(), "datePicker");
+            }
+        });
+
+        dropDate.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                whichEditTextIsPressed = 1;
+                return false;
+            }
+        });
+
+        endDate.setOnTouchListener(new View.OnTouchListener()
+        {
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+                whichEditTextIsPressed = 2;
+                return false;
+            }
+        });
+
+        confirmRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle(); bundle.putString("value", "Uspesno ste rezervisali parking mesto!");
+                startActivity(new Intent(getApplicationContext(), MainPageActivity.class).putExtras(bundle));
+                finish();
             }
         });
     }
@@ -54,6 +113,18 @@ public class ReservationActivity extends AppCompatActivity implements DatePicker
 
     @Override
     public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, minute);
+        String date = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)) + "/" + String.valueOf(calendar.get(Calendar.MONTH)) + "/" + String.valueOf(calendar.get(Calendar.YEAR));
+        String time = String.valueOf(calendar.get(Calendar.HOUR_OF_DAY)) + ":" + String.valueOf(calendar.get(Calendar.MINUTE)) + "h";
+
+        //provjera datuma
+
+        if(whichEditTextIsPressed == 1)
+            dropDate.setText(date + "  " + time);
+        else if(whichEditTextIsPressed == 2)
+            endDate.setText(date + "  " + time);
 
     }
+
 }
