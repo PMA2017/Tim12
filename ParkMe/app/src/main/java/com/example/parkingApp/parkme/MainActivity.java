c7c547fc9647c72663567682111cc6c5276e816c
Bosko Sogura <bolesogura93@gmail.com>
2017-04-23 14:41:32 +0200

    Ispravak linka.

--- ParkMe/app/src/main/java/com/example/parkingApp/parkme/MainActivity.java
+++ ParkMe/app/src/main/java/com/example/parkingApp/parkme/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.facebook.FacebookSdk;
 
@@ -16,12 +17,21 @@ public class MainActivity extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
+        TextView reg = (TextView) findViewById(R.id.link_signup);
         Button login = (Button) findViewById(R.id.sign_in_button);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent in = new Intent(getApplicationContext(),MainPageActivity.class);
                 startActivity(in);
+            }
+        });
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in2 = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(in2);
             }
         });
     }
